2f0395b57f0b1dbb5683f6fb548eb23f6e9f5705
carolina-guzman <carolina.guzman5480@alumnos.udg.mx>
2020-10-14 23:24:07 -0500

    funcion brubuja terminada

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,21 @@
 package main
 
-func Burbuja(s []int64)  {
+import "fmt"
 
+func Burbuja(s []int64) {
+	for i := 1; i < (len(s)); i++ {
+		for j := 0; j < (len(s) - 1); j++ {
+			if s[j] > s[j+1] {
+				s[j], s[j+1] = s[j+1], s[j]
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+	s := []int64{4, 3, 2, 1, 5, 0}
+	fmt.Println(s)
+	Burbuja(s)
+	fmt.Println(s)
+
 }
